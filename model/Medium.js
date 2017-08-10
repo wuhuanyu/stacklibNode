2fafffc804321e606d34d2c4c39ef85fe332c259
吴环宇 <1790949749@qq.com>
2017-08-10 21:05:31 +0800

    medium api

--- model/Medium.js
+++ model/Medium.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise=global.Promise;
-const Schema =mongoose.Schema;
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+
+
+const MediumSchema = new Schema({
+    source: String,
+    crawled_at: Number,
+    title: String,
+    url: String,
+    text: [],
+    image_urls: []
+});
+
+
+const Medium = mongoose.model('medium', MediumSchema);
+
+Medium.fields = [
+    'source', 'crawled_at', 'title', 'text', 'image_urls', 'id'
+]
+
+
+Medium.findByTitle = function (title, fields) {
+    return Medium.find({ title: title })
+        .select(fields.join(' '))
+}
+
+
+Medium.findBeforeTime = (time,fields)=>{
+
+
+}
 
